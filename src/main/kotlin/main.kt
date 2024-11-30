1f706aacd0679771eb9f3398647a7895d9a47029
Ilya Prokopenko <ilya_prokopenko@mail.ru>
2024-11-30 22:38:21 +0300

    2.2 №1 completed by

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,52 @@
 class main {
 }
 
+data class Post(
+    val id: Long = 0,
+    val ownerId: Long = 0,
+    val fromId: Long = 0,
+    val createdBy: Long = 0,
+    val date: Long = 0,
+    val text: String = "",
+    val replyOwnerId: Long = 0,
+    val replyPostId: Long = 0,
+    val friendsOnly: Boolean = false,
+    val comments: Comments = Comments(),
+    val likes: Likes = Likes(),
+    val reposts: Reposts = Reposts(),
+    val views: Views = Views(),
+    val postType: PostType = PostType.POST
+)
+
+data class Comments(
+    val count: Int = 0,
+    val canPost: Boolean = false,
+    val groupsCanPost: Boolean = false,
+    val canClose: Boolean = false,
+    val canOpen: Boolean = false
+)
+
+data class Likes(
+    val count: Int = 0,
+    val userLikes: Boolean = false,
+    val canLike: Boolean = false,
+    val canPublish: Boolean = false
+)
+
+data class Reposts(
+    val count: Int = 0,
+    val userReposted: Boolean = false
+)
+
+data class Views(
+    val count: Int = 0
+)
+
+enum class PostType {
+    POST, COPY, REPLY, POSTPONE, SUGGEST
+}
+
+
+class WallService {
+    private var posts = emptyArray<Post>()
+}
